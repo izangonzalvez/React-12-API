afe4b0ec40d3dc02faffe86529176d74860458d8
Izan <izgoar@fp.insjoaquimmir.cat>
2024-03-07 19:03:46 +0100

    Post edit acabat

--- geomir23/src/posts/PostList.jsx
+++ geomir23/src/posts/PostList.jsx
@@ -5,9 +5,7 @@ import { UserContext } from '../userContext';
 
 export const PostList = ({v, deletePost}) => {
   
-    let { authToken } = useContext(UserContext)
-
-    let usuari = authToken.email
+    let { usuari, authToken } = useContext(UserContext)
 
     let img = "https://backend.insjoaquimmir.cat/storage/" + v.file.filepath
     
@@ -41,7 +39,7 @@ export const PostList = ({v, deletePost}) => {
         </td>
         <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
         <Link to={"/posts/"+v.id} className="w-max text-cyan-600"> 👁️ </Link>
-        { v.author.email === usuari ? 
+        { v.author.name === usuari ? 
         (
             <>
               
